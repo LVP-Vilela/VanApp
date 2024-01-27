41e0e0b63f551fe024c9ca8966e306dae3fc51f7
LVP-Vilela <157846659+LVP-Vilela@users.noreply.github.com>
2024-01-27 18:49:50 -0300

    Start commit

---
+++ src/main/java/br/com/tcc/vanApp/VanAppApplication.java
@@ -0,0 +1,13 @@
+package br.com.tcc.vanApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VanAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VanAppApplication.class, args);
+	}
+
+}
