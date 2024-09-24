f7c309c4c0075f47d7eb760c29e55dc9a0e0f68e
Vilela <lvp.lucasvilelapiza@gmail.com>
2024-09-23 21:56:54 -0300

    atualizações front end

--- front-end/js/script.js
+++ front-end/js/script.js
@@ -57,3 +57,50 @@ document.addEventListener('DOMContentLoaded', () => {
   getListEscolas();
   getListBairros();
 });
+
+
+function openOption(evt, option) {
+  var i, x, tablinks;
+  x = document.getElementsByClassName("option");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < x.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" w3-border-yellow", "");
+  }
+  document.getElementById(option).style.display = "block";
+  evt.currentTarget.firstElementChild.className += " w3-border-yellow";
+
+  document.getElementById("Cadastro2").style.display = "none";
+  document.getElementById("Cadastro2").style.visibility = "hidden";
+}
+
+function openCondutor() {
+  document.getElementById("cliente").style.visibility = "hidden";
+  document.getElementById("condutor").style.visibility = "visible";
+}
+
+function openCliente() {
+  document.getElementById("condutor").style.visibility = "hidden";
+  document.getElementById("cliente").style.visibility = "visible";
+}
+
+function pesquisar() {
+  document.getElementById("Condutores").style.display = "block";
+  document.getElementById("Condutores").style.visibility = "visible";
+  window.location.href = "#Pesquisa";
+}
+
+function cadastrar1() {
+  document.getElementById("Cadastro").style.display = "none";
+  document.getElementById("Cadastro2").style.display = "block";
+  document.getElementById("Cadastro2").style.visibility = "visible";
+}
+
+function cancelCadastro() {
+  document.getElementById("Cadastro2").style.display = "none";
+  document.getElementById("Cadastro2").style.visibility = "hidden";
+  document.getElementById("Cadastro").style.display = "block";
+  document.getElementById("Cadastro").style.visibility = "visible";
+}
